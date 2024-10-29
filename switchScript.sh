57972da85cdb0df9db0c36a91fc8d6c8f80186bc
Huangqian <bluefire1982@gmail.com>
2024-10-29 19:50:43 +0800

    update 20241029

--- switchScript.sh
+++ switchScript.sh
@@ -551,14 +551,12 @@ curl -sL https://api.github.com/repos/impeeza/sys-patch/releases/latest \
 curl -sL https://api.github.com/repos/impeeza/sys-patch/releases/latest \
   | grep -oP '"browser_download_url": "\Khttps://[^"]*sys-patch[^"]*.zip"' \
   | sed 's/"//g' \
-  | xargs -I {} curl -sL {} -o sys-patch-zip.zip
+  | xargs -I {} curl -sL {} -o sys-patch.zip
 if [ $? -ne 0 ]; then
     echo "sys-patch download\033[31m failed\033[0m."
 else
     echo "sys-patch download\033[32m success\033[0m."
-    unzip -oq sys-patch-zip.zip
     unzip -oq sys-patch.zip
-    rm sys-patch-zip.zip
     rm sys-patch.zip
 fi
 
