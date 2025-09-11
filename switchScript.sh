6330a42cecef8d04acd339b49ac446acfc34dce4
Huangqian <bluefire1982@gmail.com>
2025-09-11 22:44:52 +0800

    update 20250911

--- switchScript.sh
+++ switchScript.sh
@@ -67,7 +67,7 @@ curl -sL "$download_url" -o hekate.zip && {
     rm hekate.zip
 } || echo "Hekate + Nyx CHS download\033[31m failed\033[0m."
 
-### Fetch Sigpatches from https://hackintendo.com/download/sigpatches
+### Fetch Sigpatches from https://gbatemp.net/threads/sigpatches-for-atmosphere-hekate-fss0-fusee-package3.571543/
 curl -sL https://raw.githubusercontent.com/huangqian8/SwitchPlugins/main/plugins/sigpatches.zip -o sigpatches.zip
 if [ $? -ne 0 ]; then
     echo "sigpatches download\033[31m failed\033[0m."
